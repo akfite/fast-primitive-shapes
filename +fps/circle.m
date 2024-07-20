5b4da35aa3ba740da7b2ab466877d28c9285584e
Austin Fite <akfite@gmail.com>
2024-07-20 14:17:10 -0400

    Introduce fps.circle

---
+++ +fps/circle.m
@@ -0,0 +1,51 @@
+function [xdata, ydata] = circle(x, y, radius, varargin)
+%CIRCLE Create data for plotting circles.
+%
+%   Usage:
+%
+%       [xdata, ydata] = FPS.CIRCLE(x, y)
+%       [xdata, ydata] = FPS.CIRCLE(x, y, radius)
+%       [xdata, ydata] = FPS.CIRCLE(x, y, radius, N)
+%
+%   Inputs:
+%
+%       x, y <numeric vector>
+%           - the center coordinates for each circle
+%
+%       radius <numeric vectors>
+%           - the radius of each circle
+%
+%       N (=100) <1x1 integer>
+%           - the number of points to use to plot each circle
+%           - affects the smoothness
+%           - must be greater than or equal to 4 (to prevent degenerate shapes)
+%
+%   Outputs:
+%
+%       xdata, ydata <numeric matrix>
+%           - one column for each circle, with a NaN as the last element
+%           - you can use these arrays directly with plot() and line(), however
+%             take note of the difference in how to make the call:
+%
+%                   plot(xdata, ydata)        -->   multiple line objects (slow)
+%                   plot(xdata(:), ydata(:))  -->   single line object    (fast)
+%
+%           - the data is provided as a matrix (i.e. prior to flushing with (:))
+%             so that the user has the flexibility to apply transformations, or
+%             just to plot as multiple objects as some applications require
+%
+%   Examples:
+%
+%       [xd, yd] = FPS.CIRCLE(1:100, 1:100, 10*rand(1,100));
+%
+%       figure; plot(xd, yd); title('multiple line objects (slow)');
+%       figure; plot(xd(:), yd(:)); title('single line object (fast)');
+%
+%   See also FPS.ELLIPSE
+
+%   Author:     Austin Fite
+%   Contact:    akfite@gmail.com
+
+    [xdata, ydata] = fps.ellipse(x, y, radius, radius, varargin{:});
+
+end
