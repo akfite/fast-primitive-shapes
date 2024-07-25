ad8f1ec2798e430602acbe8535e8cdb8b0f1df3b
Austin Fite <akfite@gmail.com>
2024-07-25 08:44:54 -0400

    Assertion for fps/point

--- +fps/point.m
+++ +fps/point.m
@@ -1,9 +1,15 @@
 function varargout = point(pt)
 %POINT Reshapes vector data into a form ready to use with plot, plot3, etc
 
+    arguments
+        pt(:,:) {mustBeReal}
+    end
+
+    assert(size(pt,2) <= 3 && size(pt,2) >= 1, ...
+        'Expected the matrix of vectors to be Nx2 or Nx3.');
+
     for i = size(pt,2):-1:1
         varargout{i} = pt(:,i);
     end
 
 end
-
